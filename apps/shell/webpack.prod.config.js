15222431cfe4d8a307978c9eac703fbd5e0a936b
luisdcsoft <luis.benavides@dreamcodesoft.com>
2022-10-05 19:49:29 -0500

    Test deploy

--- apps/shell/webpack.prod.config.js
+++ apps/shell/webpack.prod.config.js
@@ -2,6 +2,9 @@ const { withModuleFederation } = require('@nrwl/angular/module-federation');
 const config = require('./module-federation.config');
 module.exports = withModuleFederation({
   ...config,
+  remotes: [
+    ['policy-management', 'http://localhost:8080/policy-management']
+  ]
   /*
    * Remote overrides for production.
    * Each entry is a pair of an unique name and the URL where it is deployed.
