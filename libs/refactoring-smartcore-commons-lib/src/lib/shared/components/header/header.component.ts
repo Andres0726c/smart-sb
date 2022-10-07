029229cf730ca6499ad94f303ea1001f4485dd76
BrahyanArteaga-SB <brahyan.arteaga@segurosbolivar.com>
2022-10-07 11:16:37 -0500

    Header and filter-policy-top

--- libs/refactoring-smartcore-commons-lib/src/lib/shared/components/header/header.component.ts
+++ libs/refactoring-smartcore-commons-lib/src/lib/shared/components/header/header.component.ts
@@ -1,6 +1,7 @@
 /* eslint-disable @typescript-eslint/no-empty-function */
 import { Component, OnInit } from '@angular/core';
 import { MenuItem } from 'primeng/api';
+import { NavigationEnd, Router } from '@angular/router';
 
 @Component({
   selector: 'refactoring-smartcore-mf-header',
@@ -14,23 +15,39 @@ export class HeaderComponent implements OnInit {
   userSesion = 'carlos.correa@dreamcodesoft.com';
   rolSesion = 'TLN';
   company = 'Seguros Bolívar';
+  flagUrl = true;
 
-  constructor() {}
-
+  constructor(public router: Router) {
+    router.events.forEach((event) => {
+      if (event instanceof NavigationEnd) {
+        if (this.router.url !== '/') {
+          this.items = [
+            {
+              label: 'Inicio',
+              icon: 'pi pi-home',
+              routerLink: '/',
+              visible: true,
+            },
+          ];
+        } else {
+          this.items = [
+            {
+              label: 'Inicio',
+              icon: 'pi pi-home',
+              routerLink: '/',
+              visible: false,
+            },
+          ];
+        }
+      }
+    });
+  }
   ngOnInit() {
-    this.items = [
-      {
-        label: 'Inicio',
-        icon: 'pi pi-home',
-        routerLink: '/'
-      },
-    ];
-
     this.items2 = [
       {
         icon: 'pi pi-sign-in',
         label: 'Cerrar sesión',
-        routerLink: '/'
+        routerLink: '/',
       },
     ];
   }
