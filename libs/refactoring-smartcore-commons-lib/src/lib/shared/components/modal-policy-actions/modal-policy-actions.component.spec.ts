c4d7bc328f12be37a2094818a49c3f2c416deeee
kathe-cr <leidy.castelblanco@segurosbolivar.com>
2022-10-14 11:56:15 -0500

    Test unitario crecion componente modal-policy-actions

--- libs/refactoring-smartcore-commons-lib/src/lib/shared/components/modal-policy-actions/modal-policy-actions.component.spec.ts
+++ libs/refactoring-smartcore-commons-lib/src/lib/shared/components/modal-policy-actions/modal-policy-actions.component.spec.ts
@@ -1,19 +1,36 @@
+import { FormBuilder } from '@angular/forms';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import {
+  DialogService,
+  DynamicDialogConfig,
+  DynamicDialogRef,
+} from 'primeng/dynamicdialog';
 import { ModalPolicyActionsComponent } from './modal-policy-actions.component';
+import { ConsultPolicyService } from 'apps/policy-management/src/app/containers/policy-management/components/consult-policy/services/consult-policy.service';
+import { HttpClientModule } from '@angular/common/http';
+import { MessageService } from 'primeng/api';
 
 describe('ModalPolicyActionsComponent', () => {
   let component: ModalPolicyActionsComponent;
+  let consultPolicyService: ConsultPolicyService;
   let fixture: ComponentFixture<ModalPolicyActionsComponent>;
 
   beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ModalPolicyActionsComponent],
-    }).compileComponents();
-
-    fixture = TestBed.createComponent(ModalPolicyActionsComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
+    consultPolicyService = ConsultPolicyService.prototype;
+    TestBed.configureTestingModule({
+      declarations: [],
+      providers: [
+        ModalPolicyActionsComponent,
+        DynamicDialogRef,
+        DynamicDialogConfig,
+        FormBuilder,
+        ModalPolicyActionsComponent,
+        DialogService,
+        MessageService,
+      ],
+      imports: [HttpClientModule],
+    });
+    component = TestBed.inject(ModalPolicyActionsComponent);
   });
 
   it('should create', () => {
