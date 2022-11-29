553d6a1f6e88b3c374077dc6d272c9bb8a47c930
karol.rodallega@segurosbolivar.com-Desarrolladora <108030407+karolrt@users.noreply.github.com>
2022-11-29 18:55:40 -0500

    pruebas unitarias-50

--- projects/policy-management/src/app/containers/main/components/modify-policy/modify-data/modify-data.component.spec.ts
+++ projects/policy-management/src/app/containers/main/components/modify-policy/modify-data/modify-data.component.spec.ts
@@ -1,19 +1,33 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { RouterTestingModule } from "@angular/router/testing";
+import { HttpClientTestingModule } from "@angular/common/http/testing";
+import { FormBuilder } from '@angular/forms';
 import { ModifyDataComponent } from './modify-data.component';
+import { ProductService } from 'projects/policy-management/src/app/core/services/product/product.service';
 
 describe('ModifyDataComponent', () => {
   let component: ModifyDataComponent;
   let fixture: ComponentFixture<ModifyDataComponent>;
+  let productService: ProductService;
+  let formBuilderMock = new FormBuilder();
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ ModifyDataComponent ]
+      imports: [
+        RouterTestingModule.withRoutes([]),
+        HttpClientTestingModule
+      ],
+      declarations: [ ModifyDataComponent ],
+      providers: [
+        { provide: FormBuilder, useValue: formBuilderMock },
+        ProductService
+      ]
     })
     .compileComponents();
 
     fixture = TestBed.createComponent(ModifyDataComponent);
     component = fixture.componentInstance;
+    productService = TestBed.inject(ProductService);
     fixture.detectChanges();
   });
 
--- projects/policy-management/src/app/containers/main/components/modify-policy/modify-policy.component.spec.ts
+++ projects/policy-management/src/app/containers/main/components/modify-policy/modify-policy.component.spec.ts
@@ -1,23 +1,142 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { ProductService } from 'projects/policy-management/src/app/core/services/product/product.service';
+import { HttpClientTestingModule } from "@angular/common/http/testing";
 import { ModifyPolicyComponent } from './modify-policy.component';
+import { RouterTestingModule } from "@angular/router/testing";
+import { FormArray, FormBuilder, FormGroup } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
+import { of, Subscription } from 'rxjs';
+import { Product } from 'projects/policy-management/src/app/core/interfaces/product/product';
+import { ResponseDTO } from 'projects/policy-management/src/app/core/interfaces/commun/response';
 
 describe('ModifyPolicyComponent', () => {
   let component: ModifyPolicyComponent;
   let fixture: ComponentFixture<ModifyPolicyComponent>;
+  let productService: ProductService;
+  let formBuilderMock = new FormBuilder();
+  let router: Router;
+
 
   beforeEach(async () => {
+
     await TestBed.configureTestingModule({
-      declarations: [ ModifyPolicyComponent ]
+      imports: [
+        RouterTestingModule.withRoutes([]),
+        HttpClientTestingModule
+      ],
+      declarations: [ModifyPolicyComponent],
+      providers: [
+        { provide: FormBuilder, useValue: formBuilderMock },
+        ProductService,
+        FormBuilder,
+        {
+          provide: FormArray,
+          useValue: {},
+        },
+        {
+          provide: FormGroup,
+          useValue: {},
+        },
+      ]
+
     })
-    .compileComponents();
+      .compileComponents();
+
 
     fixture = TestBed.createComponent(ModifyPolicyComponent);
     component = fixture.componentInstance;
+    productService = TestBed.inject(ProductService);
+    router = TestBed.inject(Router);
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('policyDataControls', () => {
+    let policyNumber = component.formPolicy.controls['policyNumber']; 
+    let group: any = component.fb.array([]);
+
+
+    component.getFieldsControls(group);
+    expect(policyNumber).toBeFalsy(); 
+    component.getGroupsControls(group);
+    expect(policyNumber).toBeFalsy(); 
+    component.getFieldsControlss(group);
+    expect(policyNumber).toBeFalsy(); 
+    
+    component.addControls(group);
+    expect(policyNumber).toBeFalsy(); 
+
+
+    
+  });
+
+  it('cancelModification', () => {
+    const spy = jest.spyOn(router, 'navigate').mockImplementation();
+    component.cancelModification()
+    expect(spy).toBeCalled();
+  });
+
+  it('getProduct', () => {
+
+    const res: ResponseDTO<Product> = {
+      body: {
+        id: 7,
+        nmName: "nombre",
+        dsDescription: "descripcion",
+        nmHashCode: 3000
+      },
+      dataHeader: {
+        code: 200,
+        status: 'OK',
+        errorList: [],
+        hasErrors: false,
+        currentPage: 9,
+        totalPage: 22,
+        totalRecords: 106,
+      },
+    };
+
+
+    let idProduct = 7;
+    const spy = jest.spyOn(productService, 'getProductById').mockReturnValue(of(res));
+    const spy2 = jest.spyOn(component, 'fillGroupData').mockImplementation();
+    const spy3 = jest.spyOn(component, 'fillRiskData').mockImplementation();
+
+    component.getProduct(idProduct)
+    expect(spy).toBeCalled();
+  });
+  it('getPolicy', () => {
+    const res: ResponseDTO<any> = {
+      body: ['policy'],
+      dataHeader: {
+        code: 200,
+        status: 'OK',
+        errorList: [],
+        hasErrors: false,
+        currentPage: 9,
+        totalPage: 22,
+        totalRecords: 106,
+      },
+    };
+    let isLoading = true;
+    let idPolicy = 100;
+    const spy = jest.spyOn(productService, 'findByIdPolicy').mockReturnValue(of(res));
+    const spy2 = jest.spyOn(component, 'mapData').mockImplementation();
+    const spy3 = jest.spyOn(component, 'getProduct').mockImplementation();
+    component.getPolicy();
+    expect(spy).toBeCalled();
+  });
+
+  it('fillRiskData',()=>{
+    let riskTypes:any="";
+    let risksArrayData: any = component.fb.array([]);
+    component.fillRiskData(riskTypes);
+  });
+
+  it('saveModification',()=>{
+  component.saveModification();
   });
 });
