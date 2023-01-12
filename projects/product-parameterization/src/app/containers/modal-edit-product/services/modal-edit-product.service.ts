cd2e8c5cdaa81af3f594d7f8d70f88c29f7330af
kathe-cr <leidy.castelblanco@segurosbolivar.com>
2023-01-12 08:55:42 -0500

    consulta de productos con servicio paginado y busqueda

--- projects/product-parameterization/src/app/containers/modal-edit-product/services/modal-edit-product.service.ts
+++ projects/product-parameterization/src/app/containers/modal-edit-product/services/modal-edit-product.service.ts
@@ -26,12 +26,11 @@ export class ModalEditProductService {
 
   }
 
-   getProductRamo=(serviceData: string='',id:string=''): Observable<any> => {
-    return id!=''?this.httpClient.get<any>(`${this._urlMS}${serviceData}/${id}`,{
+   getProductRamo=(search: string='0', page: number = 0, size:number = 0): Observable<any> => {
+    search= search == ""?'0':search;
+    return this.httpClient.get<any>(`${this._urlMS}/findByName/${page}/${size}/${search}`,{
       headers: this._httpHeaders,
-    }) : this.httpClient.get<any>(`${this._urlMS}${serviceData}`,{
-      headers: this._httpHeaders,
-    });
+    })
   }
 
   validProduct=(serviceData: string='',id:string=''): Observable<any> => {
