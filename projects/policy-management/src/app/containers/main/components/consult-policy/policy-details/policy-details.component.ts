722004db99ce433159134ae2168eef5176a988a2
HernanBR <96083065+HernanBR@users.noreply.github.com>
2023-01-16 10:40:26 -0500

    Ajuste policy details

--- projects/policy-management/src/app/containers/main/components/consult-policy/policy-details/policy-details.component.ts
+++ projects/policy-management/src/app/containers/main/components/consult-policy/policy-details/policy-details.component.ts
@@ -11,6 +11,11 @@ export class PolicyDetailsComponent implements OnInit {
   component: any;
   isLoading = true;
   policy!: Policy
+  petData: any=
+    {
+    petType:"",
+    petBrand:""
+    };
   constructor(public ref: DynamicDialogRef, public config: DynamicDialogConfig, public consultPolicyService: ConsultPolicyService) { }
 
   ngOnInit(): void {
@@ -18,6 +23,18 @@ export class PolicyDetailsComponent implements OnInit {
       next: (res) => {
         if (res.body) {
           this.policy = res.body
+
+          let dataRisk = this.policy.productFactory.nmContent?.riskTypes[0].complementaryData[1].fields;
+
+          let petTypeLst = JSON.parse(dataRisk.find((f:any) => f.businessCode=== "TIPO_MASCOTA")?.domainList.valueList);
+          let petBrandLst = JSON.parse(dataRisk.find((f:any) => f.businessCode=== "RAZA")?.domainList.valueList);
+
+          this.petData={
+            petType: petTypeLst.find((x:any)=>x.code === this.policy.complementaryData.petType)?.description,
+            petBrand: petBrandLst.find((x:any)=>x.code === this.policy.complementaryData.petBrand)?.description,
+          }
+
+         
         }
         this.isLoading = false
       },
