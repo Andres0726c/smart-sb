17d032b6c99c51f2062fd39367ad835d75a20b40
HernanBR <96083065+HernanBR@users.noreply.github.com>
2023-01-20 14:48:24 -0500

    Ajuste modify

--- projects/policy-management/src/app/containers/main/components/modify-policy/modify-policy.component.ts
+++ projects/policy-management/src/app/containers/main/components/modify-policy/modify-policy.component.ts
@@ -75,6 +75,7 @@ export class ModifyPolicyComponent {
   url: any = "";
   types: any = [];
   optionsAux: any = [];
+  BusinessCode ='datos_basicos';
   constructor(
     private confirmationService: ConfirmationService,
     public productService: ProductService,
@@ -401,6 +402,7 @@ export class ModifyPolicyComponent {
           console.log(this.policyAux.plcy.rsk['1'].rskDtGrp,"risk");
           console.log(level);
           console.log(this.policyAux);
+        
           
           if (level === "policy"){
             value = this.policyAux.plcy.plcyDtGrp[group.code][businessCode] ;
@@ -467,9 +469,32 @@ export class ModifyPolicyComponent {
 
   }
 
+  validData(){
+
+    let flagValid = false;
+
+    for (let key of Object.values(this.policy.plcy.plcyDtGrp[this.BusinessCode])) {
+        if (key !== null){
+            flagValid = true;
+            break;
+        }
+    }
+    return flagValid;
+  }
+
   savePolicyModify() {
     this.isSaving = true
+
+
+    if(!this.validData()){
+
+    this.policy.plcy.plcyDtGrp[this.BusinessCode] = this.policyAux.plcy.plcyDtGrp[this.BusinessCode];
+    this.policy.plcy.rsk['1'].rskDtGrp[this.BusinessCode] =  this.policyAux.plcy.rsk['1'].rskDtGrp[this.BusinessCode]
+   
+    }
+
     this.productService.saveModify(this.policy)
+    
       .subscribe((resp: any) => {
 
         if (resp.dataHeader.code != 500) {
