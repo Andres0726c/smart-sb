a95c6b774907e9a4e178f4acd32ff16aa78aa01d
luisdcsoft <luis.benavides@dreamcodesoft.com>
2023-05-05 14:55:08 -0500

    Ajuste major bug

--- libs/commons-lib/src/lib/components/header/header.component.ts
+++ libs/commons-lib/src/lib/components/header/header.component.ts
@@ -21,7 +21,38 @@ export class HeaderComponent implements OnInit {
   closing = false;
 
   constructor(public router: Router, private cognitoService: CognitoService) {
-    router.events.forEach((event) => {
+    this.items2 = [
+      {
+        icon: 'pi pi-sign-in',
+        label: 'Cerrar sesión',
+        command: async() => {
+         await this.signOut().then().catch();
+        }
+      },
+    ];
+  }
+
+  async ngOnInit() {
+    this.cognitoService
+      .getUser()
+      .then((value) => {
+        this.company = JSON.parse(value.attributes['custom:sessionInformation']).businessName;
+        this.userSesion = value.username;
+        this.rolSesion = value.signInUserSession.accessToken.payload['cognito:groups'];
+        this.isAuthenticated = true;
+      })
+      .catch((err) => {
+        this.isAuthenticated = false;
+        this.cognitoService.signOut()
+        .then(async () => {
+          await this.router.navigate(['/autenticacion']).then();
+        })
+        .catch((err) => {
+          console.error('Error al cerrar sesión');
+        });
+      });
+    
+    await this.router.events.forEach((event) => {
       if (event instanceof NavigationEnd) {
         if (this.router.url !== '/inicio') {
           this.sessionLocation = '- Gestión de Póliza';
@@ -46,36 +77,6 @@ export class HeaderComponent implements OnInit {
         }
       }
     }).then();
-
-    this.items2 = [
-      {
-        icon: 'pi pi-sign-in',
-        label: 'Cerrar sesión',
-        command: async() => {
-         await this.signOut().then().catch();
-        }
-      },
-    ];
-  }
-  ngOnInit() {
-    this.cognitoService
-      .getUser()
-      .then((value) => {
-        this.company = JSON.parse(value.attributes['custom:sessionInformation']).businessName;
-        this.userSesion = value.username;
-        this.rolSesion = value.signInUserSession.accessToken.payload['cognito:groups'];
-        this.isAuthenticated = true;
-      })
-      .catch((err) => {
-        this.isAuthenticated = false;
-        this.cognitoService.signOut()
-        .then(async () => {
-          await this.router.navigate(['/autenticacion']).then();
-        })
-        .catch((err) => {
-          console.error('Error al cerrar sesión');
-        });
-      });
   }
 
   async signOut(){
